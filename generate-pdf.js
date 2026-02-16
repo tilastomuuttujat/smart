b0aa53d9c1c25ab127e48c2fb528758a39f8963d
tilastomuuttujat <55614962+tilastomuuttujat@users.noreply.github.com>
2026-02-16 11:48:38 +0200

    Create generate-pdf.js

---
+++ generate-pdf.js
@@ -0,0 +1,40 @@
+import puppeteer from "puppeteer";
+import path from "path";
+
+const filePath = path.resolve("./ekirja.html");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ["--no-sandbox", "--font-render-hinting=medium"]
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(`file://${filePath}`, {
+    waitUntil: "networkidle0"
+  });
+
+  await page.emulateMediaType("print");
+
+  await page.pdf({
+    path: "Kirja_A5_PRINT.pdf",
+    printBackground: true,
+    preferCSSPageSize: true,
+    displayHeaderFooter: true,
+    headerTemplate: `<div></div>`,
+    footerTemplate: `
+      <div style="font-size:9px;width:100%;text-align:center;">
+        <span class="pageNumber"></span>
+      </div>`,
+    margin: {
+      top: "20mm",
+      bottom: "20mm",
+      left: "0mm",
+      right: "0mm"
+    }
+  });
+
+  await browser.close();
+  console.log("A5 painokelpoinen PDF luotu.");
+})();
